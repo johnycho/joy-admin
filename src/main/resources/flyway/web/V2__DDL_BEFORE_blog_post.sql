56fffbb7eb388303e469fcef9ca5e1fc06c8e765
johnycho <shunnn215@gmail.com>
2025-07-08 14:41:45 +0900

    :wrench: 블로그 컨텐츠 컬럼 사이즈 변경

--- src/main/resources/flyway/web/V2__DDL_BEFORE_blog_post.sql
+++ src/main/resources/flyway/web/V2__DDL_BEFORE_blog_post.sql
@@ -6,11 +6,12 @@ CREATE TABLE IF NOT EXISTS blog_post
     title         VARCHAR(255) COLLATE utf8mb4_bin NULL,
     authors       VARCHAR(255) COLLATE utf8mb4_bin NULL,
     tags          VARCHAR(255) COLLATE utf8mb4_bin NULL,
-    contents      TEXT COLLATE utf8mb4_bin         NULL,
+    contents      LONGTEXT     COLLATE utf8mb4_bin NULL,
     created_at    datetime                         NOT NULL,
     modified_at   datetime                         NOT NULL,
     CONSTRAINT pk_blog_post PRIMARY KEY (id),
     CONSTRAINT uk_blog_post_uuid UNIQUE (uuid)
+    CONSTRAINT uk_blog_post_slug UNIQUE (slug)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_bin;
